c8d1790bf2f6fbb91aaf30ca28e7241c6beab7d2
Nam DO <thanhnamdo203@gmail.com>
2021-10-27 20:26:15 +0200

    commit 27-10

--- Main.js
+++ Main.js
@@ -38,10 +38,22 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
 
-// const dishGridEl = Array.from(document.querySelectorAll('#dishGrid'));
-// if (dishGridEl.length > 0){
-//   // console.log(dishGridEl)
-//   dishGridEl.forEach(item => {
-//     item.setAttribute('data-aos', 'fade-up');
-//   })
-// }
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
